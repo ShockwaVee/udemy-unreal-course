662ca5a365746508006417e1cc75fad130414eb9
Davor Tvoric <davor.tvoric@gmail.com>
2020-01-05 12:41:26 +0100

    Add initial values for the game

--- Triple X/triplex.cpp
+++ Triple X/triplex.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+    std::cout << "You are a thief trying to get the gold from a vault!";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to get access...";
+
+    const int a = 0;
+    const int b = 1;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+    
+    std::cout << std::endl;
+    std::cout << sum  << std::endl;
+    std::cout << product << std::endl;
+    
     return 0;
 }
